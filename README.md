6e178d6d51324f709c383676a67a37c95c5cb31e
Angel <angel_epa_luses@hotmail.com>
2019-02-26 06:51:27 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Onhover image zoom
+
+The code make zoom on the image and place the zoomin in the wished position of the screen
+
+
+## Slideshow samples
+---
+
+
+![sample #1](https://github.com/angelparkour499/javascript-slideshow/blob/master/Slideshow_sample.png)
+![sample #2](https://github.com/angelparkour499/javascript-slideshow/blob/master/Slideshow_sample%232.png)
+
+
+*It can be used on a slideshow as well*
---
+++ zoom.js
@@ -0,0 +1,13 @@
+function zoomIn(event) {
+  var element = document.getElementById("overlay");
+  element.style.display = "inline-block";
+  var img = document.getElementById("imgZoom");
+  var posX = event.offsetX ? (event.offsetX) : event.pageX - img.offsetLeft;
+  var posY = event.offsetY ? (event.offsetY) : event.pageY - img.offsetTop;
+  element.style.backgroundPosition=(-posX*5)+"px "+(-posY*5)+"px";
+}
+
+function zoomOut() {
+var element = document.getElementById("overlay");
+  element.style.display = "none";
+}
